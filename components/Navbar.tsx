0a94d591d78b2733e8c8229ee3c1b5cb407085d9
LiponRoy <liponbdbd@gmail.com>
2023-09-17 17:33:44 +0600

    responsive navbar and footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,43 +1,69 @@
 "use client";
+import { navBarLinks } from "@/constants";
+import Image from "next/image";
 import Link from "next/link";
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
-const allLinks = [
-  {
-    id: 1,
-    title: "Home",
-    url: "/",
-  },
-  {
-    id: 4,
-    title: "About",
-    url: "/about",
-  },
-  {
-    id: 5,
-    title: "Contact",
-    url: "/contact",
-  },
-];
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  const toggleController = () => {
+    setToggle(!toggle);
+  };
   return (
-    <div className=' bg-blue-600 text-white  '>
-      <div className="flex-between py-4 px-10">
-      <Link href='/' className="px-4">
-      <span>Logo</span>
-        </Link>
-      
-      <div className="">
-        {allLinks.map((link)=>(
-          <Link key={link.id} href={link.url} className="px-4">
-          {link.title}
-        </Link>
-        ))}
+    <div>
+      <div className="navContainer">
+        <div className=" flex items-center justify-between w-full h-full ">
+          {/* left texts */}
+          <div className="logo">
+          <Link href='/' >
+        <Image
+          src='/logo.svg'
+          alt='logo'
+          width={60}
+          height={60}
+          className='object-contain'
+        />
+      </Link>
+          </div>
+          {/* middle texts */}
+          <div className="navItem">
+            <div className=" cursor-pointer">--</div>
+            <div className=" cursor-pointer">--</div>
+          </div>
+          {/* right texts */}
+          <div className="navItem">
+            {navBarLinks.map((link) => (
+              <Link key={link.id} href={link.url} className="px-4">
+                {link.title}
+              </Link>
+            ))}
+          </div>
+          {/* for mobile */}
+          <div onClick={toggleController} className="toggleBtn">
+            {toggle ? <FaTimes></FaTimes> : <FaBars></FaBars>}
+          </div>
+          {/* \toggleContainerHidden */}
+          {toggle && (
+            <div className="toggleContainer">
+              {navBarLinks.map((link) => (
+                <Link
+                  key={link.id}
+                  href={link.url}
+                  className="px-4"
+                  onClick={toggleController}
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
-      </div>
-      
-      </div>
+    </div>
+  );
+};
 
-  )
-}
 
-export default Navbar
+
+export default Navbar;
